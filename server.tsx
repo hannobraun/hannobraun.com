959cc8ab92ff5e86ebf5dda4511b8e0e3c94972d
Hanno Braun <hello@hannobraun.com>
2024-05-06 20:12:04 +0200

    Serve HTML

--- server.tsx
+++ server.tsx
@@ -1,9 +1,27 @@
+/** @jsx jsx */
+
+import { html } from "hono/helper.ts";
 import { Hono } from "hono/mod.ts";
+import { jsx } from "hono/middleware.ts";
 
 const app = new Hono();
 
+const Layout = () =>
+  html`<!DOCTYPE html>
+  <html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <title>stuff.hannobraun.com</title>
+    </head>
+    <body>
+      <p>Hello, world!</p>
+    </body>
+  </html>`;
+
 app.get("/", (c) => {
-  return c.text("Hello, world!");
+  return c.html(<Layout />);
 });
 
 Deno.serve(app.fetch);
