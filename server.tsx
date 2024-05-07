8f11266cfc046df5770411ee0ebb776b2e267323
Hanno Braun <hello@hannobraun.com>
2024-05-07 22:09:56 +0200

    Redirect from `hannobraun.deno.dev`

--- server.tsx
+++ server.tsx
@@ -5,7 +5,16 @@ import { Hono } from "hono/mod.ts";
 // import { jsx } from "hono/middleware.ts";
 import { serveStatic } from "hono/middleware.ts";
 
-const app = new Hono();
+const app = new Hono({
+  getPath: (req) => req.url.replace(/^https?:\/(.+?)$/, "$1"),
+});
+
+// This is the default domain for the Deno Deploy project. I don't want to use
+// that.
+app.all("/hannobraun.deno.dev/:path", (c) => {
+  const { path } = c.req.param();
+  return c.redirect(`https://archive.hannobraun.com/${path}`, 308);
+});
 
 app.use("*", serveStatic({ root: "./archive.hannobraun.com" }));
 
