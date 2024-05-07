3641c3894fd4f70beea661b7ebd94e3943f872db
Hanno Braun <hello@hannobraun.com>
2024-05-07 21:20:40 +0200

    Serve archive files

--- server.tsx
+++ server.tsx
@@ -1,27 +1,30 @@
 /** @jsx jsx */
 
-import { html } from "hono/helper.ts";
+// import { html } from "hono/helper.ts";
 import { Hono } from "hono/mod.ts";
-import { jsx } from "hono/middleware.ts";
+// import { jsx } from "hono/middleware.ts";
+import { serveStatic } from "hono/middleware.ts";
 
 const app = new Hono();
 
-const Layout = () =>
-  html`<!DOCTYPE html>
-  <html lang="en">
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+app.use("*", serveStatic({ root: "./archive.hannobraun.com" }));
 
-        <title>stuff.hannobraun.com</title>
-    </head>
-    <body>
-      <p>Hello, world!</p>
-    </body>
-  </html>`;
+// const Layout = () =>
+//   html`<!DOCTYPE html>
+//   <html lang="en">
+//     <head>
+//         <meta charset="UTF-8" />
+//         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-app.get("/", (c) => {
-  return c.html(<Layout />);
-});
+//         <title>stuff.hannobraun.com</title>
+//     </head>
+//     <body>
+//       <p>Hello, world!</p>
+//     </body>
+//   </html>`;
+
+// app.get("/", (c) => {
+//   return c.html(<Layout />);
+// });
 
 Deno.serve(app.fetch);
