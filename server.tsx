65a673387292601b12fab1cdf72e10e8cd22e70f
Hanno Braun <hello@hannobraun.com>
2024-05-07 23:45:39 +0200

    Rewrite server without Hono

--- server.tsx
+++ server.tsx
@@ -1,54 +1,29 @@
-/** @jsx jsx */
+import { serveDir } from "https://deno.land/std@0.224.0/http/file_server.ts";
 
-// import { html } from "hono/helper.ts";
-import { Hono } from "hono/mod.ts";
-// import { jsx } from "hono/middleware.ts";
-import { serveStatic } from "hono/middleware.ts";
+Deno.serve((req) => {
+  const url = new URL(req.url);
 
-const app = new Hono({
-  getPath: (req) => req.url.replace(/^https?:\/(.+?)$/, "$1"),
+  // This is the default domain for the Deno Deploy project. I don't want to use
+  // that.
+  if (url.hostname == "hannobraun.deno.dev") {
+    return Response.redirect(
+      `https://archive.hannobraun.com${url.pathname}`,
+      308,
+    );
+  }
+
+  // These are legacy domains that I want to redirect 1:1 to the new domain.
+  if (url.hostname == "archive.braun-odw.eu") {
+    return Response.redirect(
+      `https://archive.hannobraun.com${url.pathname}`,
+      308,
+    );
+  }
+
+  // Primary domain. Serve from the respective directory.
+  if (url.hostname == "archive.hannobraun.com") {
+    return serveDir(req, { fsRoot: "archive.hannobraun.com" });
+  }
+
+  return new Response("not found", { status: 404 });
 });
-
-// This is the default domain for the Deno Deploy project. I don't want to use
-// that.
-app.all("/hannobraun.deno.dev/:path", (c) => {
-  const { path } = c.req.param();
-  return c.redirect(`https://archive.hannobraun.com/${path}`, 308);
-});
-app.all("/hannobraun.deno.dev", (c) => {
-  return c.redirect(`https://archive.hannobraun.com/`, 308);
-});
-
-// Legacy domain
-app.all("/archive.braun-odw.eu/:path", (c) => {
-  const { path } = c.req.param();
-  return c.redirect(`https://archive.hannobraun.com/${path}`, 308);
-});
-app.all("/archive.braun-odw.eu", (c) => {
-  return c.redirect(`https://archive.hannobraun.com/`, 308);
-});
-
-app.use(
-  "/archive.hannobraun.com/*",
-  serveStatic({ root: "./" }),
-);
-
-// const Layout = () =>
-//   html`<!DOCTYPE html>
-//   <html lang="en">
-//     <head>
-//         <meta charset="UTF-8" />
-//         <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-//         <title>stuff.hannobraun.com</title>
-//     </head>
-//     <body>
-//       <p>Hello, world!</p>
-//     </body>
-//   </html>`;
-
-// app.get("/", (c) => {
-//   return c.html(<Layout />);
-// });
-
-Deno.serve(app.fetch);
