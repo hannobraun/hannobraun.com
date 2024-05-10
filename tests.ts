acd19892f3ec2fb55520f77a863a850bf3a439fc
Hanno Braun <hello@hannobraun.com>
2024-05-10 22:32:28 +0200

    Add test

--- tests.ts
+++ tests.ts
@@ -2,6 +2,16 @@ import { assertEquals } from "std/assert/mod.ts";
 
 import { handler } from "./handler.ts";
 
+Deno.test("serve archive", async () => {
+  const req = new Request("https://archive.hannobraun.com/missile-miner/");
+  const res = await handler(req);
+
+  assertEquals(res.status, 200);
+
+  // Consume response, so test doesn't leak resources.
+  await res.text();
+});
+
 Deno.test("redirect default domain", async () => {
   const req = new Request("https://hannobraun.deno.dev");
   const res = await handler(req);
