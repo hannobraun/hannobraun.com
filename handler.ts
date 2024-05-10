216ac91f87314dde6f1b9cc481370f858223dde2
Hanno Braun <hello@hannobraun.com>
2024-05-11 00:27:04 +0200

    Refactor

--- handler.ts
+++ handler.ts
@@ -58,12 +58,15 @@ const serveStatic = (hostname: string) => {
 
 const redirect = {
     permanent: (sources: string[], target: string) => {
+        return redirect.withCode(sources, target, 308);
+    },
+    withCode: (sources: string[], target: string, code: number) => {
         return (request: Request, url: URL) => {
             for (const source of sources) {
                 if (url.hostname == source) {
                     return Response.redirect(
                         `https://${target}${url.pathname}`,
-                        308,
+                        code,
                     );
                 }
             }
