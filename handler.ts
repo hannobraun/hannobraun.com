94a657cd01f4fb82ae32c68348af3eab722877e5
Hanno Braun <hello@hannobraun.com>
2024-05-10 23:45:55 +0200

    Update indent width

--- handler.ts
+++ handler.ts
@@ -1,72 +1,73 @@
 import { serveDir } from "std/http/file_server.ts";
 
 export const handler = (request: Request) => {
-  const pipeline = new Pipeline(request)
-    .on_request((request, url) =>
-      serveStatic("archive.hannobraun.com", request, url)
-    ).on_request((request, url) =>
-      redirect("hannobraun.deno.dev", archive.hostname, request, url)
-    ).on_request((request, url) =>
-      redirect("archive.braun-odw.eu", archive.hostname, request, url)
-    );
+    const pipeline = new Pipeline(request)
+        .on_request((request, url) =>
+            serveStatic("archive.hannobraun.com", request, url)
+        ).on_request((request, url) =>
+            redirect("hannobraun.deno.dev", archive.hostname, request, url)
+        ).on_request((request, url) =>
+            redirect("archive.braun-odw.eu", archive.hostname, request, url)
+        );
 
-  if (
-    pipeline.request instanceof Response || pipeline.request instanceof Promise
-  ) {
-    return pipeline.request;
-  }
+    if (
+        pipeline.request instanceof Response ||
+        pipeline.request instanceof Promise
+    ) {
+        return pipeline.request;
+    }
 
-  return new Response("not found", { status: 404 });
+    return new Response("not found", { status: 404 });
 };
 
 class Pipeline {
-  request: PipelineRequest;
-  url: URL;
+    request: PipelineRequest;
+    url: URL;
 
-  constructor(request: Request) {
-    this.request = request;
-    this.url = new URL(request.url);
-  }
-
-  on_request(f: (request: Request, url: URL) => PipelineRequest): Pipeline {
-    if (this.request instanceof Request) {
-      this.request = f(this.request, this.url);
+    constructor(request: Request) {
+        this.request = request;
+        this.url = new URL(request.url);
     }
 
-    return this;
-  }
+    on_request(f: (request: Request, url: URL) => PipelineRequest): Pipeline {
+        if (this.request instanceof Request) {
+            this.request = f(this.request, this.url);
+        }
+
+        return this;
+    }
 }
 
 type PipelineRequest = Request | Response | Promise<Response>;
 
 const serveStatic = (
-  hostname: string,
-  req: Request,
-  url: URL,
+    hostname: string,
+    req: Request,
+    url: URL,
 ) => {
-  if (url.hostname == hostname) {
-    return serveDir(req, { fsRoot: hostname });
-  }
+    if (url.hostname == hostname) {
+        return serveDir(req, { fsRoot: hostname });
+    }
 
-  return req;
+    return req;
 };
 
 const redirect = (
-  source: string,
-  target: string,
-  req: Request | Response | Promise<Response>,
-  url: URL,
+    source: string,
+    target: string,
+    req: Request | Response | Promise<Response>,
+    url: URL,
 ) => {
-  if (url.hostname == source) {
-    return Response.redirect(
-      `https://${target}${url.pathname}`,
-      308,
-    );
-  }
+    if (url.hostname == source) {
+        return Response.redirect(
+            `https://${target}${url.pathname}`,
+            308,
+        );
+    }
 
-  return req;
+    return req;
 };
 
 const archive = {
-  hostname: "archive.hannobraun.com",
+    hostname: "archive.hannobraun.com",
 };
--- tests.ts
+++ tests.ts
@@ -3,29 +3,35 @@ import { assertEquals } from "std/assert/mod.ts";
 import { handler } from "./handler.ts";
 
 Deno.test("serve archive", async () => {
-  const req = new Request("https://archive.hannobraun.com/missile-miner/");
-  const res = await handler(req);
+    const req = new Request("https://archive.hannobraun.com/missile-miner/");
+    const res = await handler(req);
 
-  assertEquals(res.status, 200);
+    assertEquals(res.status, 200);
 
-  // Consume response, so test doesn't leak resources.
-  await res.text();
+    // Consume response, so test doesn't leak resources.
+    await res.text();
 });
 
 Deno.test("redirect default domain", async () => {
-  const req = new Request("https://hannobraun.deno.dev");
-  const res = await handler(req);
+    const req = new Request("https://hannobraun.deno.dev");
+    const res = await handler(req);
 
-  assertEquals(res.status, 308);
-  assertEquals(res.headers.get("location"), "https://archive.hannobraun.com/");
+    assertEquals(res.status, 308);
+    assertEquals(
+        res.headers.get("location"),
+        "https://archive.hannobraun.com/",
+    );
 });
 
 Deno.test("redirect legacy archive domain", async () => {
-  const req = new Request("https://archive.braun-odw.eu");
-  const res = await handler(req);
+    const req = new Request("https://archive.braun-odw.eu");
+    const res = await handler(req);
 
-  assertEquals(res.status, 308);
-  assertEquals(res.headers.get("location"), "https://archive.hannobraun.com/");
+    assertEquals(res.status, 308);
+    assertEquals(
+        res.headers.get("location"),
+        "https://archive.hannobraun.com/",
+    );
 });
 
 // I've had some bad experiences with framework in the past, so let's just make
@@ -34,31 +40,34 @@ Deno.test("redirect legacy archive domain", async () => {
 // that.
 
 Deno.test("redirect default domain - /a", async () => {
-  const req = new Request("https://hannobraun.deno.dev/a");
-  const res = await handler(req);
+    const req = new Request("https://hannobraun.deno.dev/a");
+    const res = await handler(req);
 
-  assertEquals(res.status, 308);
-  assertEquals(res.headers.get("location"), "https://archive.hannobraun.com/a");
+    assertEquals(res.status, 308);
+    assertEquals(
+        res.headers.get("location"),
+        "https://archive.hannobraun.com/a",
+    );
 });
 
 Deno.test("redirect default domain - /a/", async () => {
-  const req = new Request("https://hannobraun.deno.dev/a/");
-  const res = await handler(req);
+    const req = new Request("https://hannobraun.deno.dev/a/");
+    const res = await handler(req);
 
-  assertEquals(res.status, 308);
-  assertEquals(
-    res.headers.get("location"),
-    "https://archive.hannobraun.com/a/",
-  );
+    assertEquals(res.status, 308);
+    assertEquals(
+        res.headers.get("location"),
+        "https://archive.hannobraun.com/a/",
+    );
 });
 
 Deno.test("redirect default domain - /a/b/c", async () => {
-  const req = new Request("https://hannobraun.deno.dev/a/b/c");
-  const res = await handler(req);
+    const req = new Request("https://hannobraun.deno.dev/a/b/c");
+    const res = await handler(req);
 
-  assertEquals(res.status, 308);
-  assertEquals(
-    res.headers.get("location"),
-    "https://archive.hannobraun.com/a/b/c",
-  );
+    assertEquals(res.status, 308);
+    assertEquals(
+        res.headers.get("location"),
+        "https://archive.hannobraun.com/a/b/c",
+    );
 });
