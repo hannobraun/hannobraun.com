27b6f8d915eed93f5baf29ff6f45b6f181cdc0a9
Hanno Braun <hello@hannobraun.com>
2024-05-10 21:49:55 +0200

    Refactor

--- handler.ts
+++ handler.ts
@@ -4,15 +4,15 @@ export const handler = (req: Request) => {
   const url = new URL(req.url);
 
   // Primary domain. Serve from the respective directory.
-  if (url.hostname == archive) {
-    return serveDir(req, { fsRoot: archive });
+  if (url.hostname == archive.hostname) {
+    return serveDir(req, { fsRoot: archive.hostname });
   }
 
   // This is the default domain for the Deno Deploy project. I don't want to use
   // that.
   if (url.hostname == "hannobraun.deno.dev") {
     return Response.redirect(
-      `https://${archive}${url.pathname}`,
+      `https://${archive.hostname}${url.pathname}`,
       308,
     );
   }
@@ -20,7 +20,7 @@ export const handler = (req: Request) => {
   // These are legacy domains that I want to redirect 1:1 to the new domain.
   if (url.hostname == "archive.braun-odw.eu") {
     return Response.redirect(
-      `https://${archive}${url.pathname}`,
+      `https://${archive.hostname}${url.pathname}`,
       308,
     );
   }
@@ -28,4 +28,6 @@ export const handler = (req: Request) => {
   return new Response("not found", { status: 404 });
 };
 
-const archive = "archive.hannobraun.com";
+const archive = {
+  hostname: "archive.hannobraun.com",
+};
