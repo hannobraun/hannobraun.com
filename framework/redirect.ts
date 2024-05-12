6dfe46be941ec938754b31267e6b373efde6c49c
Hanno Braun <hello@hannobraun.com>
2024-05-12 22:02:07 +0200

    Refactor

--- framework/redirect.ts
+++ framework/redirect.ts
@@ -1,22 +1,23 @@
 export const redirect = {
     permanent: (sources: string[], target: string) => {
-        return redirect.withCode(sources, target, 308);
+        return redirectWithCode(sources, target, 308);
     },
     temporary: (sources: string[], target: string) => {
-        return redirect.withCode(sources, target, 307);
+        return redirectWithCode(sources, target, 307);
     },
-    withCode: (sources: string[], target: string, code: number) => {
-        return (request: Request, url: URL) => {
-            for (const source of sources) {
-                if (url.hostname == source) {
-                    return Response.redirect(
-                        `https://${target}${url.pathname}`,
-                        code,
-                    );
-                }
-            }
+};
 
-            return request;
-        };
-    },
+const redirectWithCode = (sources: string[], target: string, code: number) => {
+    return (request: Request, url: URL) => {
+        for (const source of sources) {
+            if (url.hostname == source) {
+                return Response.redirect(
+                    `https://${target}${url.pathname}`,
+                    code,
+                );
+            }
+        }
+
+        return request;
+    };
 };
