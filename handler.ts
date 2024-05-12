8d75c9708aee276b6feaffaf8181dc5d362d5fa6
Hanno Braun <hello@hannobraun.com>
2024-05-12 22:01:09 +0200

    Move redirect code to dedicate module

--- handler.ts
+++ handler.ts
@@ -1,5 +1,7 @@
 import { serveDir } from "std/http/file_server.ts";
 
+import { redirect } from "./framework/redirect.ts";
+
 export const handler = (request: Request) => {
     const response = new Pipeline(request)
         .on_request(
@@ -54,29 +56,6 @@ const serveStatic = (hostname: string) => {
 
         return request;
     };
-};
-
-const redirect = {
-    permanent: (sources: string[], target: string) => {
-        return redirect.withCode(sources, target, 308);
-    },
-    temporary: (sources: string[], target: string) => {
-        return redirect.withCode(sources, target, 307);
-    },
-    withCode: (sources: string[], target: string, code: number) => {
-        return (request: Request, url: URL) => {
-            for (const source of sources) {
-                if (url.hostname == source) {
-                    return Response.redirect(
-                        `https://${target}${url.pathname}`,
-                        code,
-                    );
-                }
-            }
-
-            return request;
-        };
-    },
 };
 
 const not_found = () => {
